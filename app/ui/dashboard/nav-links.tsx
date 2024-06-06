e308aee312f5da4cb23563225687a98150b73b61
Josh Frankel <joshmfrankel@gmail.com>
2024-06-06 10:54:08 -0400

    Fix Typescript errors

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -24,7 +24,7 @@ const links = [
 export default function NavLinks() {
   const pathname = usePathname();
 
-  const linkClasses = (href) => {
+  const linkClasses = (href: string) => {
     return clsx(
       "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
       {
